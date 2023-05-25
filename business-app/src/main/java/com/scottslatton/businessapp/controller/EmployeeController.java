6b09a2d9ac7677fd08b3e3302d357d5f936e3675
ScottSlatton <scottslatton.tech@gmail.com>
2023-05-25 09:19:43 -0500

    created angular project

--- business-app/src/main/java/com/scottslatton/businessapp/controller/EmployeeController.java
+++ business-app/src/main/java/com/scottslatton/businessapp/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.scottslatton.businessapp.controller;
 import com.scottslatton.businessapp.repository.EmployeeRepo;
 import com.scottslatton.businessapp.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/v1/")
 public class EmployeeController {
 
